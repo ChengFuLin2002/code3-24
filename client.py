708a25f23cc67b2c905b727d869a6237ad23809c
David Lin <davidlin@DaviddeMacBook-Air-2.local>
2025-03-24 16:04:39 +0800

    修改連線

--- client.py
+++ client.py
@@ -1,6 +1,4 @@
-#client portion
 import socket
-import sys
 
 class GameClient:
     def __init__(self, host='10.22.215.154', port=63569):
@@ -19,15 +17,14 @@ class GameClient:
     def start_game(self):
         try:
             while True:
-                # 接收伺服器訊息
                 data = self.client_socket.recv(1024).decode('utf-8')
                 if not data:
                     break
                     
                 print(data)
-                
-                # 檢查是否需要輸入
-                if "請選擇" in data or "選擇行動" in data:
+
+                # 只有在自己回合時才輸入
+                if "選擇行動" in data:
                     user_input = input(">>> ")
                     self.client_socket.send(user_input.encode('utf-8'))
                 
@@ -39,7 +36,7 @@ class GameClient:
 if __name__ == "__main__":
     client = GameClient()
     
-    print("歡迎來到回合制戰鬥！")
+    print("歡迎來到 1v1 回合制對戰！")
     if client.connect():
         print("連線成功！")
         client.start_game()
